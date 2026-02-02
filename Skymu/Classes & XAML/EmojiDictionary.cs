66870194bd5fb89c5130ebea2b31d11084e5a0d2
OmegaAOL <hackersword666@gmail.com>
2026-02-02 14:10:06 -0800

    ba

---
+++ Skymu/Classes & XAML/EmojiDictionary.cs
@@ -0,0 +1,224 @@
+﻿/*==========================================================*/
+// Skymu is copyrighted by The Skymu Team.
+// You may contact The Skymu Team at contact@skymu.app.
+/*==========================================================*/
+// Modification or redistribution of this code is contingent
+// on your agreement to be bound by the terms of our License.
+// If you do not wish to abide by those terms, you may not
+// use, modify, or distribute any code from the Skymu project.
+// License: http://skymu.app/license.txt
+/*==========================================================*/
+using System.Collections.Generic;
+
+public static class EmojiDictionary
+{
+    public static readonly Dictionary<string, string> Map =
+        new Dictionary<string, string>
+        {
+            // Faces and emotions
+            { "1F620", "angry" },
+            { "1F607", "angel" },
+            { "1F633", "blush" },
+            { "1F60E", "cool" },
+            { "1F622", "cry" },
+            { "1F62D", "cry" },
+            { "1F608", "devil" },
+            { "1F922", "disgust" },
+            { "1F610", "dull" },
+            { "1F614", "emo" },
+            { "1F612", "envy" },
+            { "1F926", "facepalm" },
+            { "1F628", "fear" },
+            { "1F92D", "giggle" },
+            { "1F60D", "hearteyes" },
+            { "1F912", "ill" },
+            { "1F970", "inlove" },
+            { "1F602", "laugh" },
+            { "1F618", "kiss" },
+            { "1F606", "laugh" },
+            { "1F913", "nerdy" },
+            { "1F61E", "sadness" },
+            { "1F644", "sarcastic" },
+            { "1F631", "shock" },
+            { "1F600", "smile" },
+            { "1F60F", "smirk" },
+            { "1F636", "speechless" },
+            { "1F62E", "surprised" },
+            { "1F914", "think" },
+            { "1F62A", "tired" },
+            { "1F61B", "tongueout" },
+            { "1F611", "unamused" },
+            { "1F609", "wink" },
+            { "1F61F", "worry" },
+            { "1F971", "yawn" },
+            
+            // Hand gestures
+            { "1F44F", "clap" },
+            { "1F91E", "fingerscrossed" },
+            { "1F44A", "fistbump" },
+            { "1F91D", "handshake" },
+            { "1F64C", "handsinair" },
+            { "1FAF6", "hearthands" },
+            { "1F64F", "praying" },
+            { "1F4AA", "muscle" },
+            { "1F44C", "ok" },
+            { "270B", "stop" },
+            { "1F91A", "talktothehand" },
+            { "270C", "victory" },
+            { "1F595", "finger" },
+            { "1F44D", "like" },
+            
+            // People and body parts
+            { "1F647", "bow" },
+            { "1F483", "dance" },
+            { "1F468", "man" },
+            { "1F937-200D-2642", "manshrug" },
+            { "1F933", "selfie" },
+            { "1F469", "woman" },
+            { "1F937-200D-2640", "womanshrug" },
+            { "1F469-200D-1F3A8", "womanartist" },
+            { "1F469-200D-1F680", "womanastronaut" },
+            { "1F6C0", "womanbath" },
+            { "1F48B", "womanblowkiss" },
+            { "1F469-200D-1F373", "womanchef" },
+            { "1F469-200D-1F4BB", "womandeveloper" },
+            { "1F469-200D-1F33E", "womanfarmer" },
+            { "1F93A", "womanfencer" },
+            { "1F469-200D-1F692", "womanfirefighter" },
+            { "1F469-200D-1F393", "womangraduate" },
+            { "1F469-200D-2695", "womanhealthworker" },
+            { "1F469-200D-1F476", "womanholdingbaby" },
+            { "1F469-200D-2696", "womanjudge" },
+            { "1F469-200D-1F527", "womanmechanic" },
+            { "1F469-200D-2708", "womanpilot" },
+            { "1F46E-200D-2640", "womanpoliceofficer" },
+            { "1F930", "womanpregnant" },
+            { "1F6B4-200D-2640", "womanridingbike" },
+            { "1F469-200D-1F52C", "womanscientist" },
+            { "1F469-200D-1F3EB", "womanteacher" },
+            { "1F931", "breastfeeding" },
+            { "1F46A", "family" },
+            { "1F917", "hug" },
+            { "1F484", "makeup" },
+            
+            // Animals
+            { "1F987", "bat" },
+            { "1F41B", "bug" },
+            { "1F430", "bunny" },
+            { "1F431", "cat" },
+            { "1F436", "dog" },
+            { "1F434", "donkey" },
+            { "1F98A", "fox" },
+            { "1F47B", "ghost" },
+            { "1F994", "hedgehog" },
+            { "1F411", "lamb" },
+            { "1F435", "monkey" },
+            { "1F427", "penguin" },
+            { "1F43B-200D-2744", "polarbear" },
+            { "1F98C", "reindeer" },
+            { "1F40F", "sheep" },
+            { "1F9A5", "sloth" },
+            { "1F40C", "snail" },
+            { "1F421", "stingray" },
+            { "1F983", "turkey" },
+            { "1F984", "unicorn" },
+            { "1F43A", "werewolf" },
+            { "1F9DF", "zombie" },
+            
+            // Food and drink
+            { "1F951", "avocadolove" },
+            { "1F37A", "beer" },
+            { "1F382", "cake" },
+            { "2615", "coffee" },
+            { "1F37E", "champagne" },
+            { "1F9C0", "cheese" },
+            { "1F9C1", "cupcake" },
+            { "1F379", "drink" },
+            { "1F967", "pie" },
+            { "1F355", "pizza" },
+            { "1F383", "pumpkin" },
+            { "1F330", "acorn" },
+            
+            // Hearts and love
+            { "1F494", "brokenheart" },
+            { "2764", "heart" },
+            { "1F49D", "lovegift" },
+            { "1F48C", "loveletter" },
+            { "1F48D", "ring" },
+            
+            // Objects
+            { "1F514", "bell" },
+            { "1F6B2", "bike" },
+            { "1F4A3", "bomb" },
+            { "1F4F7", "camera" },
+            { "1F697", "car" },
+            { "1F4BB", "computer" },
+            { "1F381", "gift" },
+            { "1F511", "key" },
+            { "1F4E7", "mail" },
+            { "1F4F1", "phone" },
+            { "2708", "plane" },
+            { "1F693", "policecar" },
+            { "1F695", "taxi" },
+            { "23F0", "time" },
+            { "1F3C6", "trophy" },
+            { "2602", "umbrella" },
+            { "1F4B0", "cash" },
+            { "1F3AF", "target" },
+            
+            // Activities
+            { "1F3C0", "basketball" },
+            { "1F3AE", "games" },
+            { "1F3A7", "headphones" },
+            { "1F3AC", "movie" },
+            { "1F3B5", "music" },
+            { "1F3C3", "running" },
+            { "1F6CD", "shopping" },
+            { "26F8", "skate" },
+            { "1F9D8", "yoga" },
+            { "1F3C8", "americanfootball" },
+            
+            // Nature and weather
+            { "1F525", "fire" },
+            { "1F386", "fireworks" },
+            { "1F338", "flower" },
+            { "1F3DD", "island" },
+            { "1F327", "rain" },
+            { "1F308", "rainbow" },
+            { "2744", "snowflake" },
+            { "1F387", "sparkler" },
+            { "2600", "sun" },
+            { "2B50", "star" },
+            
+            // Medals and awards
+            { "1F949", "bronzemedal" },
+            { "1F947", "goldmedal" },
+            { "1F948", "silvermedal" },
+            
+            // Symbols
+            { "274C", "no" },
+            { "1F4A9", "poop" },
+            { "1F480", "skull" },
+            { "2705", "yes" },
+            
+            // Misc
+            { "1F389", "party" },
+            { "1F92E", "puke" },
+            { "1F923", "rofl" },
+            { "1F634", "sleepy" },
+            { "1F92C", "swear" },
+            { "1F613", "sweat" },
+            { "1F976", "shivering" },
+            
+            // Holiday themed
+            { "1F385", "santa" },
+            { "1F384", "xmastree" },
+            { "1F9D9-200D-2640", "witch" },
+            { "1F9DB", "vampire" },
+            { "1F9DB-200D-2642", "dracula" },
+            
+            // Lip/mouth expressions
+            { "1F444", "lips" },
+            { "1F910", "lipssealed" },
+        };
+}
