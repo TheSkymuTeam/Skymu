fc5a4b8db024f327f3e3bcd78816ac1624ba54fd
OmegaAOL <hackersword666@gmail.com>
2026-02-02 11:07:34 -0800

    fsd

--- Skymu/Properties/Settings.Designer.cs
+++ Skymu/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Skymu.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
         public int WindowFrame {
             get {
                 return ((int)(this["WindowFrame"]));
