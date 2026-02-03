0d3df05447108cd956aa1de7841e7fef2eb375c5
OmegaAOL <hackersword666@gmail.com>
2026-02-03 13:35:06 -0800

    das

--- Skymu/Classes & XAML/MessageTools.cs
+++ Skymu/Classes & XAML/MessageTools.cs
@@ -17,7 +17,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
-// using Emoji.Wpf; // Color Emoji Textblock. CAUSES PERFORMANCE DELAYS
+// using Emoji.Wpf; // Color Emoji Textblock. CAUSES PERFORMANCE DELAYS, DO NOT USE
 using System.Windows.Controls; // Standard Textblock with Tahoma-rendered emoji
 using System.Windows.Documents;
 using System.Windows.Media;
@@ -27,7 +27,7 @@ namespace Skymu
 {
     internal class MessageTools
     {
-        private static bool IsEmojiTextElement(string element)
+        private static bool IsEmojiTextElement(string element) // Checks if the selected text element is an emoji or not.
         {
             bool hasEmojiRune = false;
 
@@ -40,7 +40,7 @@ namespace Skymu
 
 
                 if (
-                    (v >= 0x1F300 && v <= 0x1FAFF) ||
+                    (v >= 0x1F300 && v <= 0x1FAFF) || // all types of emoji unicode stuff
                     (v >= 0x2600 && v <= 0x26FF) ||
                     (v >= 0x2700 && v <= 0x27BF) ||
                     (v >= 0x1F1E6 && v <= 0x1F1FF)
@@ -54,24 +54,25 @@ namespace Skymu
         }
 
 
-        public static TextBlock FormTextblock(string input, bool doNotFormat = false)
+        public static TextBlock FormTextblock(string input, bool doNotFormat = false) // The main function. You put text in, completely formatted textblock comes out.
         {
             var textBlock = new TextBlock
             {
-                TextWrapping = TextWrapping.Wrap,
+                TextWrapping = TextWrapping.Wrap, // otherwise text wouldn't go to a newline unless explicitly told to
             };
 
             TextOptions.SetTextFormattingMode(textBlock, TextFormattingMode.Display);
 
-            if (doNotFormat)
+            if (doNotFormat) // Just return a plain unformatted TextBlock
             {
                 textBlock.Text = input;
                 return textBlock;
             }
 
-            var inlines = new List<Inline>();
+            var inlines = new List<Inline>(); // Create inline list, to store all the different Runs for formatted text and links and emojis and etc
             int position = 0;
 
+            // This Regular Expressions pattern determines what syntax corresponds to what group. It scans the input for these symbols and matches the associated text to the groups.
             string pattern = @"(```)(.+?)\1|(`)(.+?)\3|(\*\*\*)(.+?)\5|(\*\*)(.+?)\7|(__)(.+?)\9|(\*|_)(.+?)\11|~~(.+?)~~|(?m)^(?:\*|-)\s+(.+)|(?m)^>\s+(.+)|(?m)^(#{1,6})\s+(.+)|(?m)^\-#\s+(.+)";
 
             foreach (Match m in Regex.Matches(input, pattern, RegexOptions.Singleline))
@@ -79,7 +80,7 @@ namespace Skymu
                 if (m.Index > position)
                     AddTextOrLinkOrClickable(inlines, input.Substring(position, m.Index - position));
 
-                if (m.Groups[1].Success) // code block ```
+                if (m.Groups[1].Success) // code block (delimiter: ```)
                 {
                     var codeText = new TextBlock
                     {
@@ -100,7 +101,7 @@ namespace Skymu
                     inlines.Add(new InlineUIContainer(border));
                     inlines.Add(new LineBreak());
                 }
-                else if (m.Groups[3].Success) // code line `
+                else if (m.Groups[3].Success) // code line (delimiter: `)
                 {
                     inlines.Add(new Run(m.Groups[4].Value)
                     {
@@ -109,7 +110,7 @@ namespace Skymu
                         Foreground = Brushes.Lime
                     });
                 }
-                else if (m.Groups[5].Success) // bold italic ***
+                else if (m.Groups[5].Success) // bold italic (delimiter: ***)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, m.Groups[6].Value);
@@ -117,41 +118,41 @@ namespace Skymu
                     span.FontStyle = FontStyles.Italic;
                     inlines.Add(span);
                 }
-                else if (m.Groups[7].Success) // bold **
+                else if (m.Groups[7].Success) // bold (delimiter: **)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, m.Groups[8].Value);
                     span.FontWeight = FontWeights.Bold;
                     inlines.Add(span);
                 }
-                else if (m.Groups[9].Success) // underline __
+                else if (m.Groups[9].Success) // underline (delimiter: __)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, m.Groups[10].Value);
                     span.TextDecorations = TextDecorations.Underline;
                     inlines.Add(span);
                 }
-                else if (m.Groups[11].Success) // italic * or _
+                else if (m.Groups[11].Success) // italic (delimiters: * or _)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, m.Groups[12].Value);
                     span.FontStyle = FontStyles.Italic;
                     inlines.Add(span);
                 }
-                else if (m.Groups[13].Success) // strikethrough ~~
+                else if (m.Groups[13].Success) // strikethrough (delimiter: ~~)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, m.Groups[13].Value);
                     span.TextDecorations = TextDecorations.Strikethrough;
                     inlines.Add(span);
                 }
-                else if (m.Groups[14].Success) // list item
+                else if (m.Groups[14].Success) // list item (delimiter: *)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, Properties.Settings.Default.ListDelimiterCharacter + " " + m.Groups[14].Value);
                     inlines.Add(span);
                 }
-                else if (m.Groups[15].Success) // quote
+                else if (m.Groups[15].Success) // quote (delimiter: >)
                 {
                     var span = new Span();
                     AddTextOrLinkOrClickable(span.Inlines, "“" + m.Groups[15].Value.Trim() + "”");
@@ -159,7 +160,7 @@ namespace Skymu
                     span.Foreground = Brushes.DimGray;
                     inlines.Add(span);
                 }
-                else if (m.Groups[16].Success) // headers
+                else if (m.Groups[16].Success) // headers (delimiters: # or ## or ###)
                 {
                     var headerSpan = new Span();
                     AddTextOrLinkOrClickable(headerSpan.Inlines, m.Groups[17].Value.Trim());
@@ -174,7 +175,7 @@ namespace Skymu
                     inlines.Add(headerSpan);
                     inlines.Add(new LineBreak());
                 }
-                else if (m.Groups[18].Success) // small text line (-# ...)
+                else if (m.Groups[18].Success) // tiny text (delimiter: -#)
                 {
                     var smallSpan = new Span();
                     AddTextOrLinkOrClickable(smallSpan.Inlines, m.Groups[18].Value.Trim());
@@ -187,25 +188,27 @@ namespace Skymu
             }
 
 
-            // Add any trailing text after last match
+            // Add any trailing text after all the matches
             if (position < input.Length)
                 AddTextOrLinkOrClickable(inlines, input.Substring(position));
 
+            // Add all the emoji-fied, linked, and markdown'ed inlines to the textblock
             foreach (var inline in inlines)
                 textBlock.Inlines.Add(inline);
 
+            // Return
             return textBlock;
         }
 
 
-
-        private static void AddTextOrLinkOrClickable(ICollection<Inline> inlines, string text)
-        {
-            if (string.IsNullOrEmpty(text)) return;
-
+        // This function takes the source text and the inlines of the newly-created Span, and adds links,  ClickableItems, and animated emoticons to them. (After that, the text formatting is applied in
+        // the main method, and the span, containg formatted text, is added to the global inline list. This, and the emoji-processing function only update the inline collection, and as such, return void.
+        private static void AddTextOrLinkOrClickable(ICollection<Inline> inlines, string text) { 
+            if (string.IsNullOrEmpty(text)) return;                                           
+                                                                                               
             int position = 0;
 
-            string linkPattern = @"\[(.+?)\]\((https?://[^\s)]+)\)|((?:https?|ftp|gopher)://[^\s]+)";
+            string linkPattern = @"\[(.+?)\]\((https?://[^\s)]+)\)|((?:https?|ftp|gopher)://[^\s]+)"; // Regex for weblinks 
             char[] punctuation = new char[] { '.', ',', ';', ')', ']', '"', '\'' };
 
             while (position < text.Length)
@@ -217,7 +220,9 @@ namespace Skymu
                 string clickableMatch = null;
                 ClickableItemConfiguration clickableItem = null;
 
-                // find the next link to be parsed in the text
+                // preparation
+
+                // find and set the next link to be parsed in the text
                 foreach (Match m in Regex.Matches(text.Substring(position), linkPattern))
                 {
                     int idx = position + m.Index;
@@ -228,7 +233,7 @@ namespace Skymu
                     }
                 }
 
-                // find the next clickable to be parsed in the text (clickables defined in plugin)
+                // find and set the next clickable to be parsed in the text (clickables defined in plugin)
                 // this loop only checks for clickables in delimiters, not standalone clickables
                 foreach (var config in Universal.Plugin.ClickableConfigurations.OfType<ClickableDelimitationConfiguration>())
                 {
@@ -252,18 +257,20 @@ namespace Skymu
                     }
                 }
 
-                // process all the text between the current and the next match
+                // action
+
+                // process all text until and the next match (the emojis can't be in any of the matches, hence why it's running here)
                 if (nextIndex > position)
                 {
                     string plain = text.Substring(position, nextIndex - position);
-                    ProcessTextWithEmoji(inlines, plain);
+                    ProcessTextWithEmoji(inlines, plain); // start the emoticon adding, takes the same parameters as this function did
                     position = nextIndex;
                 }
 
                 // if the next match is a link, process it like so
                 if (nextLink != null && nextLink.Index + position == nextIndex)
                 {
-                    if (nextLink.Groups[1].Success && nextLink.Groups[2].Success)
+                    if (nextLink.Groups[1].Success && nextLink.Groups[2].Success) // Markdown-formatted links e.g. [text](link)
                     {
                         string display = nextLink.Groups[1].Value;
                         string url = nextLink.Groups[2].Value.TrimEnd(punctuation);
@@ -283,7 +290,7 @@ namespace Skymu
                     }
                     else if (nextLink.Groups[3].Success)
                     {
-                        string url = nextLink.Groups[3].Value.TrimEnd(punctuation);
+                        string url = nextLink.Groups[3].Value.TrimEnd(punctuation); // Standard links
                         if (Uri.TryCreate(url, UriKind.Absolute, out Uri uri))
                         {
                             var hyperlink = new Hyperlink(new Run(url)) { NavigateUri = uri };
@@ -303,7 +310,7 @@ namespace Skymu
                     continue;
                 }
 
-                // if the next match is a clickable, process it like so
+                // if the next match is a Clickable, process it like so
                 if (nextClickableConfig != null && clickableItem != null)
                 {
                     int start = clickableStartIndex;
@@ -322,7 +329,7 @@ namespace Skymu
                         }
                         else
                         {
-                            // no closing delimiter, fallback
+                            // if there is no closing delimiter, fallback
                             clickableText = text.Substring(start + 1, clickableMatch.Length);
                         }
                     }
@@ -333,14 +340,14 @@ namespace Skymu
                     }
 
                     var hyperlink = new Hyperlink(new Run(clickableText));
-                    // For now ignore clickable type actions
+                    // For now, ignore clickable type actions. TODO add type actions.
                     inlines.Add(hyperlink);
 
                     position = end;
                     continue;
                 }
 
-                // if nothing matched, break
+                // if nothing matched, break and add no inlines using this method
                 if (nextIndex == text.Length)
                     break;
             }
@@ -348,7 +355,7 @@ namespace Skymu
 
 
 
-        private static void ProcessTextWithEmoji(ICollection<Inline> inlines, string text)
+        private static void ProcessTextWithEmoji(ICollection<Inline> inlines, string text) // This function replaces Unicode emojis in the text with inline animated emoticons.
         {
             StringInfo info = new StringInfo(text);
             int loopCount = info.LengthInTextElements;
@@ -383,14 +390,14 @@ namespace Skymu
                         {
                             Source = sourceImg,
                             IsHitTestVisible = false,
-                            Width = 22,
+                            Width = 22, // 2px padding to fix image render clip bug
                             Height = 20,
                             ElementCount = (sourceImg.PixelHeight / 20), 
                             StackDirection = SpriteStackDirection.Vertical,
                             DefaultIndex = 0,
                             Slice = false, 
                             IsAnimation = true, 
-                            AnimationFps = 45.0
+                            AnimationFps = 45.0 // change to speed up or slow down animation
                         };
 
                         RenderOptions.SetBitmapScalingMode(sliceControl, BitmapScalingMode.NearestNeighbor);
