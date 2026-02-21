d280d1fdff6f4a8258bb14d532b204f6609216c8
OmegaAOL <hackersword666@gmail.com>
2026-02-21 03:07:01 -0800

    das

--- Plugins/Discord/Classes/API.cs
+++ Plugins/Discord/Classes/API.cs
@@ -14,6 +14,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -60,7 +61,8 @@ namespace Discord.Classes
 
         public async Task<string> SendAPI(string endpoint, HttpMethod httpMethod, string token = null, object data = null, byte[] fileData = null, string fileName = null, Dictionary<string, string> headers = null)
         {
-            string url = $"https://discord.com/api/v9/{endpoint}";
+            string url = "https://discord.com/api/v" + Discord.Core.API_VERSION + "/" + endpoint; 
+            Debug.WriteLine(url);
             var request = new HttpRequestMessage(httpMethod, url);
 
             if (!string.IsNullOrEmpty(token))
