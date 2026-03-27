cea440cf8db1ba59e5db845ca94140285da7e890
OmegaAOL <semmalkumar9@yahoo.com>
2026-03-27 05:44:28 -0700

    Add installer NSIS

--- Skymu/Properties/Settings.Designer.cs
+++ Skymu/Properties/Settings.Designer.cs
@@ -145,7 +145,7 @@ namespace Skymu.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0.3.19")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.3.20")]
         public string BuildVersion {
             get {
                 return ((string)(this["BuildVersion"]));
@@ -157,7 +157,7 @@ namespace Skymu.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Drocea Steve\'s Lava Chicken")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Drocea Union Jack Cake")]
         public string BuildName {
             get {
                 return ((string)(this["BuildName"]));
