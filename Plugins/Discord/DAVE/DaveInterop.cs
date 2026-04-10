4530bbfe71599be2a06ed122e9c3979d2bfa8a79
OmegaAOL <hackersword666@gmail.com>
2026-04-09 20:44:02 -0700

    da

--- Plugins/Discord/DAVE/DaveInterop.cs
+++ Plugins/Discord/DAVE/DaveInterop.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*==========================================================*/
+// Skymu is copyrighted by The Skymu Team.
+// You may contact The Skymu Team: skymu@hubaxe.fr.
+/*==========================================================*/
+// Modification or redistribution of this code is contingent
+// on your agreement to be bound by the terms of our License.
+// If you do not wish to abide by those terms, you may not
+// use, modify, or distribute any code from the Skymu project.
+// License: http://skymu.app/legal/licenses/standard.txt
+/*==========================================================*/
+
+using System;
 using System.Runtime.InteropServices;
 
 namespace Discord.Dave
