b95258f3819a2371891fe2321c9d803b716b9919
nilfinx <nulflox@disroot.org>
2026-04-11 17:46:59 +0900

    'Sure you want to quit Skype?' checkbox accuracy fix

--- Skymu/Skyaeris/Login.xaml.cs
+++ Skymu/Skyaeris/Login.xaml.cs
@@ -135,7 +135,7 @@ namespace Skymu.Skyaeris
         private void OnCheckUpdates(object sender, EventArgs e) { new Updater(true); }
         private void OnPrivacy(object sender, EventArgs e) { Universal.OpenUrl(Universal.SKYMU_WEBSITE_PRIVACY); }
         private void OnAbout(object sender, EventArgs e) { new About().Show(); }
-        private void OnClose(object sender, EventArgs e) { Universal.Close(); }
+        private void OnClose(object sender, EventArgs e) { Universal.Close(false); }
 
         private void Login_Loaded(object sender, EventArgs e)
         {
