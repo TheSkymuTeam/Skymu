2c05adb6ab013183f6eaafa6ce7420394e3c8940
nilfinx <nulflox@disroot.org>
2026-04-18 22:14:13 +0900

    Stop typing on message send

--- Skymu/ViewModels/MainViewModel.cs
+++ Skymu/ViewModels/MainViewModel.cs
@@ -453,6 +453,8 @@ namespace Skymu.ViewModels
         {
             if (string.IsNullOrEmpty(text) || SelectedConversation == null) return;
 
+            StopTyping();
+
             string tempId = SKYMU_SENDING + "/" + Guid.NewGuid().ToString();
             var preview = new Message(
                 tempId, Universal.Plugin.MyInformation, DateTime.Now, text, null, null
@@ -613,6 +615,11 @@ namespace Skymu.ViewModels
                 TypingText = text;
                 IsTypingVisible = true;
             }));
+        }
+
+        public void StopTyping()
+        {
+            _typingTimer.Change(0, Timeout.Infinite);
         }
 
         public void StartTyping()
