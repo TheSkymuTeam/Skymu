cfdd4f61a168506f529e60ea1a1ab65226ff7664
OmegaAOL <hackersword666@gmail.com>
2026-04-19 07:02:18 -0700

    Update licensing information

--- Skymu/ViewModels/LoginViewModel.cs
+++ Skymu/ViewModels/LoginViewModel.cs
@@ -6,7 +6,7 @@
 // on your agreement to be bound by the terms of our License.
 // If you do not wish to abide by those terms, you may not
 // use, modify, or distribute any code from the Skymu project.
-// License: http://skymu.app/legal/licenses/standard.txt
+// License: https://skymu.app/legal/license
 /*==========================================================*/
 
 /*==========================================================*/
