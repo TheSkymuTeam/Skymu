95d8f8a243e27e1a6523bc9a8ffede0b204de63f
nilfinx <nulflox@disroot.org>
2026-04-20 20:59:11 +0900

    Better notification control, supress a warning

---
+++ Skymu/Classes/NotificationTriggerType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skymu.Classes
+{
+    public enum NotificationTriggerType
+    {
+        ALL = 1,
+        PING = 2,
+        DM = 4,
+        PDM = PING | DM,
+    }
+}
