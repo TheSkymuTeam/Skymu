46dbaf37bce0318a732a7af9cc288d4c90147171
OmegaAOL <hackersword666@gmail.com>
2026-05-28 01:04:02 -0700

    crp

--- Plugins/Discord/DAVE/DaveInterop.cs
+++ Plugins/Discord/DAVE/DaveInterop.cs
@@ -16,7 +16,7 @@ namespace Discord.Dave
 {
     internal static class DaveInterop
     {
-        private const string Lib = "libdave.dll";
+        private const string LibDave = "libdave.dll";
 
         [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
         public delegate void DAVEMLSFailureCallback(
@@ -24,39 +24,39 @@ namespace Discord.Dave
             [MarshalAs(UnmanagedType.LPStr)] string reason,
             IntPtr userData);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveFree(IntPtr ptr);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveSessionCreate(
             IntPtr context,
             [MarshalAs(UnmanagedType.LPStr)] string authSessionId,
             DAVEMLSFailureCallback callback,
             IntPtr userData);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionDestroy(IntPtr session);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionInit(
             IntPtr session,
             ushort version,
             ulong groupId,
             [MarshalAs(UnmanagedType.LPStr)] string selfUserId);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionReset(IntPtr session);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionSetProtocolVersion(IntPtr session, ushort version);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionSetExternalSender(
             IntPtr session,
             [In] byte[] externalSender,
             UIntPtr length);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionProcessProposals(
             IntPtr session,
             [In] byte[] proposals,
@@ -66,13 +66,13 @@ namespace Discord.Dave
             out IntPtr commitWelcomeBytes,
             out UIntPtr commitWelcomeBytesLength);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveSessionProcessCommit(
             IntPtr session,
             [In] byte[] commit,
             UIntPtr length);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveSessionProcessWelcome(
             IntPtr session,
             [In] byte[] welcome,
@@ -80,53 +80,53 @@ namespace Discord.Dave
             IntPtr recognizedUserIds,
             UIntPtr recognizedUserIdsLength);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveSessionGetMarshalledKeyPackage(
             IntPtr session,
             out IntPtr keyPackage,
             out UIntPtr length);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveSessionGetKeyRatchet(
             IntPtr session,
             [MarshalAs(UnmanagedType.LPStr)] string userId);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool daveCommitResultIsFailed(IntPtr commitResult);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         [return: MarshalAs(UnmanagedType.I1)]
         public static extern bool daveCommitResultIsIgnored(IntPtr commitResult);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveCommitResultGetRosterMemberIds(
             IntPtr commitResult,
             out IntPtr rosterIds,
             out UIntPtr rosterIdsLength);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveCommitResultDestroy(IntPtr commitResult);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveWelcomeResultGetRosterMemberIds(
             IntPtr welcomeResult,
             out IntPtr rosterIds,
             out UIntPtr rosterIdsLength);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveWelcomeResultDestroy(IntPtr welcomeResult);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveDecryptorCreate();
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveDecryptorDestroy(IntPtr decryptor);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveDecryptorTransitionToKeyRatchet(IntPtr decryptor, IntPtr keyRatchet);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern DAVEDecryptorResultCode daveDecryptorDecrypt(
             IntPtr decryptor,
             DAVEMediaType mediaType,
@@ -136,28 +136,28 @@ namespace Discord.Dave
             UIntPtr frameCapacity,
             out UIntPtr bytesWritten);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern UIntPtr daveDecryptorGetMaxPlaintextByteSize(
             IntPtr decryptor,
             DAVEMediaType mediaType,
             UIntPtr encryptedFrameSize);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr daveEncryptorCreate();
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveEncryptorDestroy(IntPtr encryptor);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern void daveEncryptorSetKeyRatchet(IntPtr encryptor, IntPtr keyRatchet);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern UIntPtr daveEncryptorGetMaxCiphertextByteSize(
             IntPtr encryptor,
             DAVEMediaType mediaType,
             UIntPtr frameSize);
 
-        [DllImport(Lib, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LibDave, CallingConvention = CallingConvention.Cdecl)]
         public static extern DAVEEncryptorResultCode daveEncryptorEncrypt(
             IntPtr encryptor,
             DAVEMediaType mediaType,
