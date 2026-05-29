3330d2d3a7a9bcc3ae1e630a47b4a8932e0cf1d0
nilFinx <nulflox@disroot.org>
2026-05-29 23:24:18 +0900

    Options 2: Electric Boogaloo

---
+++ Skymu/Forms/Options/General/Sounds.xaml.cs
@@ -0,0 +1,23 @@
+﻿/*==========================================================*/
+// Skymu is copyrighted by The Skymu Team.
+// For any inquiries or concerns, email contact@skymu.app.
+/*==========================================================*/
+// Modification or redistribution of this code is contingent
+// on your agreement to be bound by the terms of our License.
+// If you do not wish to abide by those terms, you may not
+// use, modify, or distribute any code from the Skymu project.
+// License: https://skymu.app/legal/license
+/*==========================================================*/
+
+using System.Windows.Controls;
+
+namespace Skymu.Views.OptionPages.General
+{
+    public partial class Sounds : Page
+    {
+        public Sounds()
+        {
+            InitializeComponent();
+        }
+    }
+}
