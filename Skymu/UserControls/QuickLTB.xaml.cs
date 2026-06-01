ed8f11b1d033b8c3bba993cd4f6c461983e974ad
nilFinx <nulflox@disroot.org>
2026-06-02 00:00:33 +0900

    QuickLTB, parse Skype lang quickly + AddContact refactor

---
+++ Skymu/UserControls/QuickLTB.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Skymu.UserControls
+{
+    [ContentProperty(nameof(Bindings))]
+    public partial class QuickLTB : UserControl
+    {
+        public string Key
+        {
+            get { return (string)GetValue(KeyProperty); }
+            set { SetValue(KeyProperty, value); }
+        }
+        public static readonly DependencyProperty KeyProperty =
+            DependencyProperty.Register(
+               nameof(Key),
+               typeof(string),
+               typeof(QuickLTB),
+               new PropertyMetadata("", OnAnyChange)
+           );
+
+        public ObservableCollection<BindingBase> Bindings { get; } =
+            new ObservableCollection<BindingBase>();
+
+        public QuickLTB()
+        {
+            InitializeComponent();
+            UpdateBinding();
+            Bindings.CollectionChanged += (_, __) => UpdateBinding();
+        }
+
+        public static void OnAnyChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is QuickLTB qltb)
+                qltb.UpdateBinding();
+        }
+
+        private void UpdateBinding()
+        {
+            var converter = (IMultiValueConverter)FindResource("SkyLangParseConverter");
+            var multiBinding = new MultiBinding
+            { 
+               Converter = converter,
+               ConverterParameter = Key
+            };
+
+            if (Bindings != null)
+            {
+                foreach (var binding in Bindings)
+                {
+                    multiBinding.Bindings.Add(binding);
+                }
+            }
+
+            ContentCtrl.SetBinding(ContentControl.ContentProperty, multiBinding);
+        }
+    }
+}
