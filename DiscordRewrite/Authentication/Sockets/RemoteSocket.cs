0b17cdf9b3733ddd79550b78fa73a175099e279a
patricktbp <135556230+n1d3v@users.noreply.github.com>
2026-08-16 23:40:08 +0100

    add: Base of the Discord plugin rewrite

---
+++ DiscordRewrite/Authentication/Sockets/RemoteSocket.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+
+namespace Naticord.Networking.WebSockets
+{
+    internal class AuthSocket : IDisposable
+    {
+        #region Variables used by AuthSocket
+        // The required URL variables for this to actually work
+        private const string gatewayUrl = "wss://remote-auth-gateway.discord.gg/?v=2";
+        private const string loginEndpoint = "users/@me/remote-auth/login";
+
+        private static readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions { Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping };
+
+        // The client required for the WebSockets to function
+        private ClientWebSocket _webSocket;
+        private readonly SemaphoreSlim _sendLock = new SemaphoreSlim(1, 1);
+
+        // Cancellation token for the socket
+        private CancellationTokenSource _cancellationToken;
+
+        private readonly SynchronizationContext _syncContext;
+        private bool _disposedSocket;
+        private int _heartbeatInterval;
+
+        private readonly AsymmetricCipherKeyPair _keyPair;
+        private readonly byte[] _spkiBytes;
+
+        // All actions for the socket
+        public event Action<string> qrCodeReady;
+        public event Action<string> tokenReceived;
+        #endregion
+
+        public AuthSocket()
+        {
+            _syncContext = SynchronizationContext.Current;
+            var keyGenerator = new RsaKeyPairGenerator();
+
+            // Generate a keypair for later...
+            keyGenerator.Init(new KeyGenerationParameters(new SecureRandom(), 2048));
+            _keyPair = keyGenerator.GenerateKeyPair();
+
+            _spkiBytes = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(_keyPair.Public).GetDerEncoded();
+        }
+
+        #region Start and stop socket functions
+        public async Task StartSocket()
+        {
+            _cancellationToken = new CancellationTokenSource();
+            _webSocket = new ClientWebSocket();
+
+            // Required so Discord doesn't kick us out for being an unknown source
+            _webSocket.Options.SetRequestHeader("Origin", "https://discord.com");
+
+            // Connect to the gateway!
+            await _webSocket.ConnectAsync(new Uri(gatewayUrl), CancellationToken.None);
+            _ = Task.Run(ReceiveLoop);
+        }
+
+        public void StopSocket() => _cancellationToken?.Cancel();
+        #endregion
+
+        #region WebSocket loops
+        private async Task ReceiveLoop()
+        {
+            var byteBuffer = new byte[16384];
+            var stringBuilder = new StringBuilder();
+
+            while (_webSocket.State == WebSocketState.Open && !_cancellationToken.IsCancellationRequested)
+            {
+                stringBuilder.Clear();
+                WebSocketReceiveResult receiveResult;
+
+                do
+                {
+                    receiveResult = await _webSocket.ReceiveAsync(new ArraySegment<byte>(byteBuffer), _cancellationToken.Token);
+                    if (receiveResult.MessageType == WebSocketMessageType.Close) { return; }
+
+                    stringBuilder.Append(Encoding.UTF8.GetString(byteBuffer, 0, receiveResult.Count));
+                }
+                while (!receiveResult.EndOfMessage);
+                await HandleMessage(stringBuilder.ToString());
+            }
+        }
+
+        private async Task HeartbeatLoop()
+        {
+            await Task.Delay(_heartbeatInterval);
+            while (!_cancellationToken.IsCancellationRequested && _webSocket.State == WebSocketState.Open)
+            {
+                await SendMessage(JsonSerializer.Serialize(new Dictionary<string, object> { { "op", "heartbeat" } }, jsonOptions));
+                await Task.Delay(_heartbeatInterval, _cancellationToken.Token);
+            }
+        }
+        #endregion
+
+        #region WS message functions (Sending stuff to Discord)
+        private async Task SendInit()
+        {
+            await SendMessage(JsonSerializer.Serialize(new Dictionary<string, object>
+            {
+                { "op", "init" },
+                { "encoded_public_key", Convert.ToBase64String(_spkiBytes) }
+            }, jsonOptions));
+        }
+
+        private async Task SendNonceProof(JsonElement rootElement)
+        {
+            string encNonce = DecryptToUrlSafeBase64(rootElement.GetProperty("encrypted_nonce").GetString());
+            await SendMessage(JsonSerializer.Serialize(new Dictionary<string, object>
+            {
+                { "op", "nonce_proof" },
+                { "nonce", encNonce }
+            }, jsonOptions));
+        }
+
+        private void HandlePendingRemoteInit(JsonElement rootElement)
+        {
+            string remoteFingerprint = rootElement.GetProperty("fingerprint").GetString();
+            using (var shaHash = SHA256.Create())
+            {
+                string expectedBase = Convert.ToBase64String(shaHash.ComputeHash(_spkiBytes)).TrimEnd('=').Replace('+', '-').Replace('/', '_');
+                if (remoteFingerprint != expectedBase)
+                {
+                    StopSocket();
+                    return;
+                }
+            }
+
+            Fire(qrCodeReady, "https://discord.com/ra/" + remoteFingerprint);
+        }
+
+        private void HandlePendingTicket(JsonElement rootElement)
+        {
+            string userPayload = DecryptToUtf8(rootElement.GetProperty("encrypted_user_payload").GetString());
+
+            string[] payloadParts = userPayload.Split(':');
+            string payloadUsername = payloadParts.Length >= 4 ? payloadParts[3] : "Unknown name";
+        }
+
+        private async Task HandlePendingLogin(JsonElement rootElement)
+        {
+            string loginTicket = rootElement.GetProperty("ticket").GetString();
+            // string loginResponse = await API.Instance.SendAPI(loginEndpoint, HttpMethod.Post, data: new { loginTicket });
+
+            /*
+            using (var jsonDoc = JsonDocument.Parse(loginResponse))
+            {
+                var rootEl = jsonDoc.RootElement;
+                if (rootEl.TryGetProperty("captcha_key", out _) || rootEl.TryGetProperty("captcha_sitekey", out _))
+                {
+                    // You are now stuck in a CAPTCHA loop, good luck!
+                    // This will be implemented soon.
+                    // Fire<string>(_ => MessageBox.Show("Discord is requesting for a CAPTCHA to be completed, Naticord does not support these as of now. Please try again later.", "Naticord", MessageBoxButtons.OK, MessageBoxIcon.Error), null);
+                    return;
+                }
+                if (!rootEl.TryGetProperty("encrypted_token", out var tokenEl) || tokenEl.ValueKind != JsonValueKind.String) { return; }
+
+                Fire(tokenReceived, DecryptToUtf8(tokenEl.GetString()));
+            }
+            */
+        }
+        #endregion
+
+        #region WS message functions (The actual backend of them)
+        private async Task HandleMessage(string messageData)
+        {
+            using (var jsonDoc = JsonDocument.Parse(messageData))
+            {
+                var rootElement = jsonDoc.RootElement;
+                string opCode = rootElement.GetProperty("op").GetString() ?? "";
+
+                switch (opCode)
+                {
+                    case "hello":
+                        // Start the heartbeat loop then send the initial payload
+                        _heartbeatInterval = rootElement.GetProperty("heartbeat_interval").GetInt32();
+                        Task.Run(() => HeartbeatLoop());
+
+                        await SendInit();
+                        break;
+                    case "nonce_proof": await SendNonceProof(rootElement); break;
+                    case "pending_remote_init": HandlePendingRemoteInit(rootElement); break;
+                    case "pending_ticket": HandlePendingTicket(rootElement); break;
+                    case "pending_login": await HandlePendingLogin(rootElement); break;
+                }
+            }
+        }
+
+        private async Task SendMessage(string messageData)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(messageData);
+            await _sendLock.WaitAsync(CancellationToken.None);
+
+            try { await _webSocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None); }
+            finally { _sendLock.Release(); }
+        }
+        #endregion
+
+        #region Decrypt functions
+        private string DecryptToUrlSafeBase64(string encBase) { return Convert.ToBase64String(OaepDecrypt(encBase)).TrimEnd('=').Replace('+', '-').Replace('/', '_'); }
+        private string DecryptToUtf8(string encBase) { return Encoding.UTF8.GetString(OaepDecrypt(encBase)); }
+
+        private byte[] OaepDecrypt(string encBase)
+        {
+            var oapeEngine = new OaepEncoding(new RsaEngine(), new Org.BouncyCastle.Crypto.Digests.Sha256Digest());
+            oapeEngine.Init(false, _keyPair.Private);
+
+            byte[] encText = Convert.FromBase64String(encBase);
+            return oapeEngine.ProcessBlock(encText, 0, encText.Length);
+        }
+        #endregion
+
+        private void Fire<T>(Action<T> actionHandler, T arg)
+        {
+            if (actionHandler == null) return;
+
+            if (_syncContext != null)
+                _syncContext.Post(_ => actionHandler(arg), null);
+            else
+                actionHandler(arg);
+        }
+
+        public void Dispose()
+        {
+            if (_disposedSocket) return;
+            _disposedSocket = true;
+
+            _cancellationToken?.Cancel();
+
+            _webSocket?.Dispose();
+            _sendLock?.Dispose();
+            _cancellationToken?.Dispose();
+        }
+    }
+}
---
+++ DiscordRewrite/PluginCore.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Yggdrasil;
+using Yggdrasil.Bottles;
+using Yggdrasil.Models;
+using Yggdrasil.Enumerations;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace DiscordRewrite
+{
+    public class Core : ICore, ICall
+    {
+        #region Plugin details
+        public bool SupportsVideoCalls => false; // Will need to set to true, once calling has been implemented.
+
+        // Plugin tubes
+        public event EventHandler<DialogBottle> DialogTube;
+
+        public event EventHandler<MessageBottle> MessageTube;
+
+        public event EventHandler<ListBottle> ListTube;
+
+        public event EventHandler<CallBottle> IncomingCallTube;
+        public event EventHandler<CallBottle> CallStateChangedTube;
+
+        // Plugin information
+        public string Name { get { return "Discord"; } }
+        public string InternalName { get { return "discord-rewrite"; } }
+        public bool SupportsServers { get { return true; } }
+
+        // Plugin authentication types
+        public AuthTypeInfo[] AuthenticationTypes
+        {
+            get
+            {
+                return new[]
+                {
+                    new AuthTypeInfo(AuthenticationMethod.Password, "E-mail"),
+                    new AuthTypeInfo(AuthenticationMethod.QRCode, "QR Code"),
+                    new AuthTypeInfo(AuthenticationMethod.Token, "Token")
+                };
+            }
+        }
+        #endregion
+
+        public int TypingTimeout => throw new NotImplementedException();
+        
+        public int TypingRepeat => throw new NotImplementedException();
+
+        public ClickableConfiguration[] ClickableConfigurations => throw new NotImplementedException();
+
+        public ObservableCollection<User> TypingUsersList => throw new NotImplementedException();
+
+        public Task<SavedCredential> StoreCredential()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetQRCode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResult> Authenticate(AuthenticationMethod auth_type, string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResult> Authenticate(SavedCredential credential)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResult> AuthenticateTwoFA(string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SendMessage(string conversation_id, string text = null, Attachment attachment = null, string parent_message_id = null, bool action = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditMessage(string conversation_id, string message_id, string new_text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteMessage(string conversation_id, string message_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<DirectMessage>> FetchContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Conversation>> FetchConversations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Server>> FetchServers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ConversationItem>> FetchMessages(Conversation conversation, Fetch fetch_type = Fetch.Newest, int message_count = 50, string identifier = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetConnectionStatus(PresenceStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetMood(string status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetTyping(string idenfitier, bool typing)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActiveCall> StartCall(string convo_id, bool is_video_call, bool start_muted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ActiveCall> AnswerCall(string convo_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeclineCall(string convo_id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EndCall(ActiveCall call)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetMuted(ActiveCall call, bool muted)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SetVideoEnabled(ActiveCall call, bool enabled)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
